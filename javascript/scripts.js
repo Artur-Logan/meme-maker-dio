cedc62749285bbfb8c12d972e0cb4fcf540cee74
Artur Logan <arturlogan1ferreira@gmail.com>
2022-11-18 00:07:20 -0300

    Projeto finalizado

---
+++ javascript/scripts.js
@@ -0,0 +1,64 @@
+function enablePhotoUpload(){
+    const imageInput = document.querySelector("#image-input")
+
+    imageInput.addEventListener("change", function(){
+        const reader = new FileReader()
+
+        reader.addEventListener("load", () => {
+            const uploadImage = reader.result
+
+            changeMemePicture(uploadImage)
+        })
+
+        reader.readAsDataURL(this.files[0])
+    })
+}
+
+async function mapImageList(){
+    const memeObject = [
+    {
+        "name": "Chapolin",
+        "path": "imagens/chapolin.jpg"
+    },
+    {
+        "name": "Chloe",
+        "path": "imagens/chloe.jpg"
+    }, 
+    {
+        "name": "funny-cat1",
+        "path": "imagens/funny-cat1.png"
+    },
+    {
+        "name": "funny-cat2",
+        "path": "imagens/funny-cat2.png"
+    },
+]
+return memeObject
+}
+
+async function creatGallery(imagemList){
+    const memeSelctor = document.querySelector("#memes-list")
+
+    imagemList.forEach(picture => {
+        let newOption = document.createElement("option")
+        newOption.text = picture.name.toUpperCase()
+        newOption.value = picture.path
+        memeSelctor.appendChild(newOption)
+    });
+}
+
+async function changeMemePicture(photo){
+    let displayImage = document.querySelector("#display-image")
+    displayImage.style.backgroundImage = `url('${photo}')`
+}
+
+async function main(){
+    const memeImageList = await mapImageList()
+
+    enablePhotoUpload()
+    await creatGallery(memeImageList)
+    await changeMemePicture(memeImageList[1].path)
+}
+
+
+main()
